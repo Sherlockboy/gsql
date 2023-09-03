fb44017f58d57077be7efa89f737165766410bfc
Khurshidbek Kobilov <kabilovhurshid5@gmail.com>
2023-09-03 23:23:00 +0500

    minor fix

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-<img width="777" alt="image" src="https://github.com/Sherlockboy/gsql/assets/43229443/11c0dadb-8779-4002-81df-ab50cc97c919">
+![img.png](public/images/screenshot.png)
--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -124,7 +124,7 @@
 
                     <div class="mt-6 flex items-center justify-end gap-x-6">
                         <button type="reset" class="text-sm font-semibold leading-6 text-gray-900">Reset</button>
-                        <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+                        <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Search</button>
                     </div>
                 </form>
             </div>
